57d3878a5c6b46c0dd170dfed66a363dacd0a4ea
ScotSche_Coding <coding.scotsche@gmail.com>
2021-05-06 16:59:14 +0200

    Start implementing interaction logic

--- IoX_Web_App/app.py
+++ IoX_Web_App/app.py
@@ -3,17 +3,38 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, g
 import sqlite3
 import json
 import plotly
 import plotly.graph_objs as go
+import database as db
 
+#   Create Flask application
 app = Flask(__name__)
 
-# Make the WSGI interface available at the top level so wfastcgi can get it.
+#   Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
 
+#   Create database object
+DATABASE = 'iox.db'
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+def query_db(query, args=(), one=False):
+    cur = get_db().execute(query, args)
+    rv = cur.fetchall()
+    cur.close()
+    return (rv[0] if rv else None) if one else rv
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 
 def createOverviewGraph():
     labels = ["Working", "Maintainance", "Failure", "Not Connected"]
@@ -26,34 +47,39 @@ def createOverviewGraph():
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
     return graphJSON
 
-
 @app.route('/')
-@app.route('/dashboard')
-def dasboard():
-    """Renders a sample page."""
-    connection = sqlite3.connect("iox.db")
-    mycursor =connection.cursor()
-    mycursor.execute("SELECT * FROM Dashboard")
-    data = mycursor.fetchall()
-    connection.close()
-
+def index():
     transferredData = [];
+
+    with app.app_context():
+        data = query_db('select * from Dashboard')
+
     for row in data:
         transferredData.append((row[1], row[2], row[3], row[4], row[5]))
 
     #overview_plot = plot([Scatter(x=[1, 2, 3], y=[3, 1, 6])], output_type='div')
     overview_plot = createOverviewGraph()
+    return render_template('dashboard.html', transferredData=transferredData);
+
+@app.route('/dashboard/', methods = ['POST'])
+def dashboard():
+
+    with app.app_context():
+        data = query_db('select * from Dashboard')
+
+    transferredData = [];
+    for row in data:
+        transferredData.append((row[1], row[2], row[3], row[4], row[5]))
+
+    with app.app_context():
+        specificData = query_db('select * from Dashboard where Tag = ?', [request.form['tag']])
+
+    print(specificData)
+
+    #overview_plot = plot([Scatter(x=[1, 2, 3], y=[3, 1, 6])], output_type='div')
+    overview_plot = createOverviewGraph()
     return render_template('dashboard.html', transferredData=transferredData, overview_plot=overview_plot)
 
-@app.route('/hello')
-def hello():
-    """Renders a sample page."""
-    return render_template('index.html')
-
-@app.route('/bye')
-def bye():
-    """Renders a sample page."""
-    return "Bye World!"
 
 if __name__ == '__main__':
     import os
--- IoX_Web_App/database.py
+++ IoX_Web_App/database.py
@@ -1,37 +1,68 @@
 import sqlite3
-import base64
+
+class DBConnection:
+    instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.instance is None:
+            cls.instance = super().__new__(DBConnection)
+            return cls.instance
+        return cls.instance
+
+    def __init__(self, db_name='iox.db'):
+        self.name = db_name
+        # connect takes url, dbname, user-id, password
+        self.conn = sqlite3.connect(db_name)
+        self.cursor = self.conn.cursor()
+
+    def getAllOverviewData(self):
+        self.cursor.execute("SELECT * FROM Dashboard")
+        return self.cursor.fetchall()
 
 
-connection = sqlite3.connect("iox.db")
-print("Opened database successfully");
+#class Database:
+#    connection = sqlite3.connect("iox.db")
 
-connection.execute('''CREATE TABLE Dashboard
-         (ID INT PRIMARY KEY,
-         Image      TEXT        NOT NULL,
-         Device     TEXT        NOT NULL,
-         Tag        CHAR(5)     NOT NULL,
-         Location   CHAR(50)    NOT NULL,
-         Status     CHAR(4)     NOT NULL);''')
+#    connection.execute('''CREATE TABLE if not exists Dashboard
+#         (ID INT PRIMARY KEY,
+#         Image      TEXT        NOT NULL,
+#         Device     TEXT        NOT NULL,
+#         Tag        CHAR(5)     NOT NULL,
+#         Location   CHAR(50)    NOT NULL,
+#         Status     CHAR(4)     NOT NULL);''')
 
-print("Table created successfully");
+#    connection.commit()
+#    print("Initializing process finished")
+
+#    @classmethod
+#    def checkIfTableExists(name):
+#        connection.execute('''CREATE TABLE if not exists %s
+#         (ID INT PRIMARY KEY,
+#         record      TEXT        NOT NULL,);''' % (name))
+#        connection.commit()
+
+#    @classmethod
+#    def getAllOverviewData(object):
+#        mycursor = object.connection.cursor()
+#        mycursor.execute("SELECT * FROM Dashboard")
+#        return mycursor.fetchall()
 
 
-imagePath = 'resources/MeasuringUnit.png'
 
-connection.execute("INSERT INTO Dashboard (Image,Device,Tag,Location,Status) \
-      VALUES (?, ?, ?, ?, ?)", (imagePath, 'DeviceName', 'D-005', 'Location', '0xFF'));
+#IMAGEPATH = 'RESOURCES/MEASURINGUNIT.PNG'
 
-connection.execute("INSERT INTO Dashboard (Image,Device,Tag,Location,Status) \
-      VALUES (?, ?, ?, ?, ?)", (imagePath, 'DeviceName', 'D-005', 'Location', '0xFF'));
+#CONNECTION.EXECUTE("INSERT INTO DASHBOARD (IMAGE,DEVICE,TAG,LOCATION,STATUS) \
+#      VALUES (?, ?, ?, ?, ?)", (IMAGEPATH, 'DEVICENAME', 'D-001', 'LOCATION', '0XFF'));
 
-connection.execute("INSERT INTO Dashboard (Image,Device,Tag,Location,Status) \
-      VALUES (?, ?, ?, ?, ?)", (imagePath, 'DeviceName', 'D-005', 'Location', '0xFF'));
+#CONNECTION.EXECUTE("INSERT INTO DASHBOARD (IMAGE,DEVICE,TAG,LOCATION,STATUS) \
+#      VALUES (?, ?, ?, ?, ?)", (IMAGEPATH, 'DEVICENAME', 'D-002', 'LOCATION', '0XFF'));
 
-connection.execute("INSERT INTO Dashboard (Image,Device,Tag,Location,Status) \
-      VALUES (?, ?, ?, ?, ?)", (imagePath, 'DeviceName', 'D-005', 'Location', '0xFF'));
+#CONNECTION.EXECUTE("INSERT INTO DASHBOARD (IMAGE,DEVICE,TAG,LOCATION,STATUS) \
+#      VALUES (?, ?, ?, ?, ?)", (IMAGEPATH, 'DEVICENAME', 'D-003', 'LOCATION', '0XFF'));
 
-connection.commit()
+#CONNECTION.EXECUTE("INSERT INTO DASHBOARD (IMAGE,DEVICE,TAG,LOCATION,STATUS) \
+#      VALUES (?, ?, ?, ?, ?)", (IMAGEPATH, 'DEVICENAME', 'D-004', 'LOCATION', '0XFF'));
 
-print("Records created successfully");
+#CONNECTION.COMMIT()
 
-connection.close()
+#CONNECTION.CLOSE()
