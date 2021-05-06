a5b1b1a6109462a9942ba2277f30499c44d9832a
ScotSche_Coding <coding.scotsche@gmail.com>
2021-05-06 08:21:14 +0200

    Continue implement style and functionality

--- IoX_Web_App/app.py
+++ IoX_Web_App/app.py
@@ -4,6 +4,9 @@ It contains the definition of routes and views for the application.
 """
 
 from flask import Flask, render_template
+import sqlite3
+import base64
+
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
@@ -14,7 +17,17 @@ wsgi_app = app.wsgi_app
 @app.route('/dashboard')
 def dasboard():
     """Renders a sample page."""
-    return render_template('dashboard.html')
+    connection = sqlite3.connect("iox.db")
+    mycursor =connection.cursor()
+    mycursor.execute("SELECT * FROM Dashboard")
+    data = mycursor.fetchall()
+    connection.close()
+
+    transferredData = [];
+    for row in data:
+        transferredData.append((row[1], row[2], row[3], row[4], row[5]))
+
+    return render_template('dashboard.html', transferredData=transferredData)
 
 @app.route('/hello')
 def hello():
--- IoX_Web_App/database.py
+++ IoX_Web_App/database.py
@@ -1,20 +1,37 @@
 import sqlite3
+import base64
 
-connection = sqlite3.connect("company.db")
 
-cursor = connection.cursor()
+connection = sqlite3.connect("iox.db")
+print("Opened database successfully");
 
-sql_command = """
-CREATE TABLE employee ( 
-staff_number INTEGER PRIMARY KEY, 
-fname VARCHAR(20), 
-lname VARCHAR(30), 
-gender CHAR(1), 
-joining DATE,
-birth_date DATE);"""
+connection.execute('''CREATE TABLE Dashboard
+         (ID INT PRIMARY KEY,
+         Image      TEXT        NOT NULL,
+         Device     TEXT        NOT NULL,
+         Tag        CHAR(5)     NOT NULL,
+         Location   CHAR(50)    NOT NULL,
+         Status     CHAR(4)     NOT NULL);''')
 
-cursor.execute(sql_command)
+print("Table created successfully");
+
+
+imagePath = 'resources/MeasuringUnit.png'
+
+connection.execute("INSERT INTO Dashboard (Image,Device,Tag,Location,Status) \
+      VALUES (?, ?, ?, ?, ?)", (imagePath, 'DeviceName', 'D-005', 'Location', '0xFF'));
+
+connection.execute("INSERT INTO Dashboard (Image,Device,Tag,Location,Status) \
+      VALUES (?, ?, ?, ?, ?)", (imagePath, 'DeviceName', 'D-005', 'Location', '0xFF'));
+
+connection.execute("INSERT INTO Dashboard (Image,Device,Tag,Location,Status) \
+      VALUES (?, ?, ?, ?, ?)", (imagePath, 'DeviceName', 'D-005', 'Location', '0xFF'));
+
+connection.execute("INSERT INTO Dashboard (Image,Device,Tag,Location,Status) \
+      VALUES (?, ?, ?, ?, ?)", (imagePath, 'DeviceName', 'D-005', 'Location', '0xFF'));
 
 connection.commit()
+
+print("Records created successfully");
 
 connection.close()
