93546c7d90ca15b0826e9da715eff1b4e6c179a5
ScotSche_Coding <coding.scotsche@gmail.com>
2021-05-06 10:54:07 +0200

    Improve logic

--- IoX_Web_App/app.py
+++ IoX_Web_App/app.py
@@ -5,12 +5,26 @@ It contains the definition of routes and views for the application.
 
 from flask import Flask, render_template
 import sqlite3
-import base64
+import json
+import plotly
+import plotly.graph_objs as go
 
 app = Flask(__name__)
 
 # Make the WSGI interface available at the top level so wfastcgi can get it.
 wsgi_app = app.wsgi_app
+
+
+def createOverviewGraph():
+    labels = ["Working", "Maintainance", "Failure", "Not Connected"]
+    values = [80, 12, 2, 6]
+
+    #   Create Donut Pie Graph
+    fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
+
+    # Create subplots: use 'domain' type for Pie subplot
+    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
+    return graphJSON
 
 
 @app.route('/')
@@ -27,7 +41,9 @@ def dasboard():
     for row in data:
         transferredData.append((row[1], row[2], row[3], row[4], row[5]))
 
-    return render_template('dashboard.html', transferredData=transferredData)
+    #overview_plot = plot([Scatter(x=[1, 2, 3], y=[3, 1, 6])], output_type='div')
+    overview_plot = createOverviewGraph()
+    return render_template('dashboard.html', transferredData=transferredData, overview_plot=overview_plot)
 
 @app.route('/hello')
 def hello():
