9006bcfac28b6196c618e28c19f71f9689cfe4d9
ScotSche_Coding <coding.scotsche@gmail.com>
2021-05-07 18:25:47 +0200

    Further implement UI

--- IoX_Web_App/app.py
+++ IoX_Web_App/app.py
@@ -3,11 +3,13 @@ This script runs the application using a development server.
 It contains the definition of routes and views for the application.
 """
 
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request, Markup, g
 import sqlite3
 import json
 import plotly
-import plotly.graph_objs as go
+from plotly.offline import plot
+from plotly.graph_objs import Scatter
+import plotly.graph_objects as go
 import database as db
 
 #   Create Flask application
@@ -37,15 +39,21 @@ def close_connection(exception):
         db.close()
 
 def createOverviewGraph():
-    labels = ["Working", "Maintainance", "Failure", "Not Connected"]
-    values = [80, 12, 2, 6]
 
-    #   Create Donut Pie Graph
-    fig = go.Figure(data=[go.Pie(labels=labels, values=values, hole=.3)])
+    labels = ['Working','Maintenance','Failure','Not Connected']
+    colors = ['lightgreen', 'orange', 'red', 'gray']
+    values = [4500, 2500, 1053, 500]
 
-    # Create subplots: use 'domain' type for Pie subplot
-    graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
-    return graphJSON
+    fig =  go.Figure(data=[go.Pie(values=values, labels=labels, hole=.6)])
+
+    fig.update_traces(marker=dict(colors=colors, line=dict(color='#FFFFFF', width=2)))
+
+    fig.update_layout(title_text='Werk 1 / Anlage 1 – Übersicht Gerätestatus', title_y=1.0)
+
+    return plot(fig, output_type='div')
+
+def createEnvelopeGraph():
+    return plot([Scatter(x=[1, 2, 3], y=[3, 1, 6])], output_type='div')
 
 @app.route('/')
 def index():
@@ -57,28 +65,30 @@ def index():
     for row in data:
         transferredData.append((row[1], row[2], row[3], row[4], row[5]))
 
-    #overview_plot = plot([Scatter(x=[1, 2, 3], y=[3, 1, 6])], output_type='div')
-    overview_plot = createOverviewGraph()
-    return render_template('dashboard.html', transferredData=transferredData);
+    return render_template('dashboard.html', transferredData=transferredData, specificDate="");
 
 @app.route('/dashboard/', methods = ['POST'])
 def dashboard():
 
+    tag = request.form['tag']
+
     with app.app_context():
         data = query_db('select * from Dashboard')
+
+    with app.app_context():
+        specificData = query_db('select * from Dashboard where Tag = ?', [tag])
 
     transferredData = [];
     for row in data:
         transferredData.append((row[1], row[2], row[3], row[4], row[5]))
 
-    with app.app_context():
-        specificData = query_db('select * from Dashboard where Tag = ?', [request.form['tag']])
-
     print(specificData)
 
-    #overview_plot = plot([Scatter(x=[1, 2, 3], y=[3, 1, 6])], output_type='div')
     overview_plot = createOverviewGraph()
-    return render_template('dashboard.html', transferredData=transferredData, overview_plot=overview_plot)
+    envelope_plot = createEnvelopeGraph()
+
+    return render_template('dashboard.html', transferredData=transferredData, 
+                           overview_plot=Markup(overview_plot), specificData=specificData, envelope_plot=Markup(envelope_plot))
 
 
 if __name__ == '__main__':
